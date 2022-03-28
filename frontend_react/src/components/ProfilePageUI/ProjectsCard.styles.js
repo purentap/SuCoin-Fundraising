746776ac78c719e84b860242141dd3fcf944317f
Puren Tap <purentap@gmail.com>
2022-03-28 13:39:22 +0300

    added projects ui and project info to profile page

--- frontend_react/src/components/ProfilePageUI/ProjectsCard.styles.js
+++ frontend_react/src/components/ProfilePageUI/ProjectsCard.styles.js
@@ -1,7 +1,6 @@
 import styled from 'styled-components';
 
-export const card = styled.div`
-    border-radius: 12px;
-    box-shadow: 0 1px 8px rgba(0, 0, 0, 0.25);
+export const button = styled.div`
+
     
 }`;
